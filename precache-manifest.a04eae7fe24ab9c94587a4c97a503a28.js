95524cd50466e32a78714a3a8a9fcf45e42e4c47
erlinsamirbanegas <erlinsamirbanegas@gmail.com>
2020-05-29 15:18:03 -0600

    Updates

---
+++ precache-manifest.a04eae7fe24ab9c94587a4c97a503a28.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2433e051e246f2614102d146f14dde",
+    "url": "/smartboard/index.html"
+  },
+  {
+    "revision": "9af35f0ce82066168af2",
+    "url": "/smartboard/static/css/2.ae87030c.chunk.css"
+  },
+  {
+    "revision": "d1af1d86461bac232006",
+    "url": "/smartboard/static/css/main.7c4d18ca.chunk.css"
+  },
+  {
+    "revision": "9af35f0ce82066168af2",
+    "url": "/smartboard/static/js/2.8def32da.chunk.js"
+  },
+  {
+    "revision": "d58808196dce5806235eaf4840e7d547",
+    "url": "/smartboard/static/js/2.8def32da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa504c6c5950fec7ae6f",
+    "url": "/smartboard/static/js/3.cb2f48d9.chunk.js"
+  },
+  {
+    "revision": "d1af1d86461bac232006",
+    "url": "/smartboard/static/js/main.4c70239c.chunk.js"
+  },
+  {
+    "revision": "b869a76624e9befb2f29",
+    "url": "/smartboard/static/js/runtime-main.ff9d9ebe.js"
+  },
+  {
+    "revision": "dc0eb6bee1a1e0a6298afb6968596ffd",
+    "url": "/smartboard/static/media/image.dc0eb6be.jpg"
+  },
+  {
+    "revision": "9da57ed0fe989e343cc5ba55e6b814da",
+    "url": "/smartboard/static/media/test.9da57ed0.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smartboard/precache-manifest.569a990a372133a8edf84a4d25a05f6f.js"
+  "/smartboard/precache-manifest.a04eae7fe24ab9c94587a4c97a503a28.js"
 );
 
 self.addEventListener('message', (event) => {
